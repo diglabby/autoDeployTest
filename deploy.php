0aac33e7ba3751c086798b7955f26e3a90ee5889
Alexey Kirpichenkau <alexwish@tut.by>
2018-01-26 18:44:33 +0300

    test commit

--- deploy.php
+++ deploy.php
@@ -36,7 +36,8 @@ foreach ($allowed_ips as $allow) {
         break;
     }
 }
-if (!$allowed) {
+//if (!$allowed) {
+if(false)
 	header('HTTP/1.1 403 Forbidden');
  	echo "<span style=\"color: #ff0000\">Sorry, no hamster - better convince your parents!</span>\n";
     echo "</pre>\n</body>\n</html>";
